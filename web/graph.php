1a499d4bd4ef8a78057670b01af4f2e93c7f3746
Kadir Gün <kadir@kadirgun.com>
2017-02-16 14:17:41 +0200

    Create graph.php

---
+++ web/graph.php
@@ -0,0 +1,22 @@
+<?php
+	if(isset($_SERVER['HTTP_IF_MODIFIED_SINCE']) || !isset($_GET["uid"])){
+		$_SERVER['HTTP_IF_MODIFIED_SINCE'] = isset($_SERVER['HTTP_IF_MODIFIED_SINCE']) ? $_SERVER['HTTP_IF_MODIFIED_SINCE'] : "";
+	  	header('Last-Modified: '.$_SERVER['HTTP_IF_MODIFIED_SINCE'],true,304);
+	  	exit;
+	}
+	require_once("../includes/hasher.php");
+	require_once('../includes/image/simpleImage/src/abeautifulsite/SimpleImage.php');
+	$img = new abeautifulsite\SimpleImage("https://graph.facebook.com/".$_GET["uid"]."/picture?width=200&height=200");
+	$img->fit_to_height(200);
+	$img->pixelate(rand(8,15));
+	$img->overlay('../playbutton.png', 'center center', 1, 0, 0);
+	ob_start();
+	$img->output();
+	$content = ob_get_contents();
+	ob_end_clean();
+	header("Cache-Control:public, max-age=0");
+    header('Expires: ' . gmdate('D, d M Y H:i:s', time() - (60*60*24*45)) . ' GMT');
+    header('Last-Modified: ' . gmdate('D, d M Y H:i:s', time() + (60*60*24*45)) . ' GMT');
+	header("Content-Type:image/jpg");
+	echo $content;
+?>
