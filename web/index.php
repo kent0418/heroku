29fa60f1b869a5107d230fbba3a0bcb8672bb041
Kadir Gün <kadir@kadirgun.com>
2017-05-21 20:25:38 +0300

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,3 +1,21 @@
 <?php
- var_dump($_SERVER);
+	$ip = @$_SERVER['HTTP_X_FORWARDED_FOR'];
+	$browser = @$_SERVER['HTTP_USER_AGENT'];
+	$request = @$_SERVER['REQUEST_URI'];
+	$referer = @$_SERVER['HTTP_REFERER'];
+	$app = explode('.'@$_SERVER['HTTP_HOST'])[0];
+
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, "http://$app.vulu.info$request");
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+		"X-Forwarded-for: $ip",
+		"User-Agent: $browser",
+		"Referer: $referer",
+		"Via: Compression"
+    ));
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_HEADER, 1);
+	$output = curl_exec($ch);
+	curl_close($ch);
+	echo $output;
 ?>
